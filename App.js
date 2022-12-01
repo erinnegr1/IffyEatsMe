e7ad9d60e80f220c8a5072a6de5a35da43997305
Jenny Chen <chen.jenny15@yahoo.com>
2022-12-01 12:02:20 -0500

    moved location of expo-location to homepage

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import {HomeScreen, SetLocation, Object} from './components/allComponents'
-import * as Location from 'expo-location';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,32 +9,30 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
 
-  const [userlocation, setUserLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+  // const [userlocation, setUserLocation] = useState(null);
+  // const [errorMsg, setErrorMsg] = useState(null);
 
-  useEffect(() => {
-    (async () => {
+  // useEffect(() => {
+  //   (async () => {
       
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+  //     let { status } = await Location.requestForegroundPermissionsAsync();
+  //     if (status !== 'granted') {
+  //       setErrorMsg('Permission to access location was denied');
+  //       return;
+  //     }
 
-      let loc = await Location.getCurrentPositionAsync({});
-      console.log(loc)
-      setUserLocation(loc);
-    })();
-  }, []);
+  //     let loc = await Location.getCurrentPositionAsync({});
+  //     console.log(loc)
+  //     setUserLocation(loc);
+  //   })();
+  // }, []);
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
-
+  // let text = 'Waiting..';
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else if (userlocation) {
+  //   text = JSON.stringify(userlocation);
+  // }
 
   return (
     <NavigationContainer>
--- components/homeScreen.js
+++ components/homeScreen.js
@@ -1,55 +1,82 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
+import * as Location from 'expo-location';
 
-function HomeScreen({navigation}) {
-    const onPressHandler =() =>{
-      navigation.navigate('Location')
-      alert('Hello')
-    }
-    return (
-      <View style={styles.container}>
+function HomeScreen({ navigation }) {
+
+  const [userlocation, setUserLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let loc = await Location.getCurrentPositionAsync({});
+      console.log(loc)
+      setUserLocation(loc);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (userlocation) {
+    text = JSON.stringify(userlocation);
+  }
+
+  const onPressHandler = () => {
+    navigation.navigate('Location')
+    alert('Hello')
+  }
+  return (
+    <View style={styles.container}>
       <Image style={styles.img} source={require('../assets/Feed-Your-Hangry.png')} />
       <Text style={styles.text}>Welcome to Iffy Eats!</Text>
       <Pressable
-        style={({pressed})=>[({backgroundColor: pressed ? 'purple':'hotpink'}), styles.wrapperCustom]}
+        style={({ pressed }) => [({ backgroundColor: pressed ? 'purple' : 'hotpink' }), styles.wrapperCustom]}
         onPress={onPressHandler}
-        >
+      >
         <Text>Click to feed your hangry</Text>
       </Pressable>
       <StatusBar style="auto" />
     </View>
-    )
-  }
+  )
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    img: {
-      height: 300,
-      width: 300,
-      margin: 10
-    },
-    input: {
-      borderWidth: 3,
-      borderColor: "chartreuse",
-      fontSize: 30
-     },
-    text: {
-      margin: 10
-    },
-    button: {
-      margin: 10,
-      padding: 10
-    },
-    wrapperCustom: {
-      borderRadius: 8,
-      padding: 6
-    }
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    height: 300,
+    width: 300,
+    margin: 10
+  },
+  input: {
+    borderWidth: 3,
+    borderColor: "chartreuse",
+    fontSize: 30
+  },
+  text: {
+    margin: 10
+  },
+  button: {
+    margin: 10,
+    padding: 10
+  },
+  wrapperCustom: {
+    borderRadius: 8,
+    padding: 6
+  }
+});
 
 export default HomeScreen
