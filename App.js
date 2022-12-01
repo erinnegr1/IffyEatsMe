c849323a964a1a1a4f393c100e895014eeb1617b
Erinnegr1 <erinne.grant@gmail.com>
2022-12-01 11:12:15 -0500

    long and lat object available

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import {HomeScreen, SetLocation, Object} from './components/allComponents'
+import * as Location from 'expo-location';
 
 const Stack = createNativeStackNavigator();
 
+
+
 export default function App() {
+
+  const [userlocation, setUserLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let loc = await Location.getCurrentPositionAsync({});
+      console.log(loc)
+      setUserLocation(loc);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
