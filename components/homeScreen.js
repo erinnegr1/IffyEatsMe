37c1041daea56e67adff809682d220994417f18a
Jenny Chen <chen.jenny15@yahoo.com>
2022-12-05 13:05:43 -0500

    working on getting YELP API to fetch from inputted address

--- components/homeScreen.js
+++ components/homeScreen.js
@@ -8,10 +8,8 @@ function HomeScreen({ navigation }) {
 
   const [userlocation, setUserLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [name, setName] = useState('');
-  //const [userAddress, setAddress] = useState('')
-
-  console.log(YELP_API_KEY)
+  //const [name, setName] = useState('');
+  const [userAddress, setUserAddress] = useState(null)
 
   const onPressHandler = () => {
     (async () => {
@@ -39,12 +37,9 @@ function HomeScreen({ navigation }) {
       }
 
       let loc = await Location.getCurrentPositionAsync({});
-      console.log(loc)
       setUserLocation(loc);
     })();
   }, []);
-
-  console.log(userlocation)
 
   // function latitude() {
   //   if (userlocation) {
@@ -71,82 +66,133 @@ function HomeScreen({ navigation }) {
     navigation.navigate('Object')
   }
 
-  // const fetchName = async () => {
-  //   const response = await fetch(
-  //     `https://pokeapi.co/api/v2/pokemon/${name}`
-  //   );
-  //   const pokemon = await response.json();
-  //   console.log(pokemon);
-  // }
-  const userAddress = '40 Division Street NY NY 10002'
+  //const userAddress = '20 WEST 34th street ny ny 10001' 
   const radius = '8000'
   const YELP_API_KEY = 'gChaVU_NPBoWTsWwmuSwZ4AbrwCyPuBFw9hHIe3irRKszbN22YZGUgbAssxD-HE8VGFLnLbQhpqyEmKl45I2BcRKdr9FSQuCMOMFIu1uf3_mrPdHeUPeBxaJv5SKY3Yx'
 
-  const getYelpRestaurants = async() => {
-    const yelpUrl = `https://api.yelp.com/v3/businesses/search?location=${userAddress}&term=food, restaurants&radius=${radius}`
-    const apiOptions = {
-      headers: {
-        Authorization: `Bearer ${YELP_API_KEY}`,
-      },
+  const getYelpRestaurants = async () => {
+    //await {userAddress}
+    // console.log(userAddress)
+    if (!userlocation) {
+      const yelpUrl = `https://api.yelp.com/v3/businesses/search?location=${userAddress}&term=food, restaurants&radius=${radius}`
+      const apiOptions = {
+        headers: {
+          Authorization: `Bearer ${YELP_API_KEY}`,
+        },
+      }
+              return await fetch(yelpUrl, apiOptions)
+                .then((res) => res.json())
+                .then((json) =>
+                console.log(userAddress),
+                  console.log(json),
+                  // setRestaurantData(
+                  //   json.businesses.filter((business) =>
+                  //     business.transactions.includes(activeTab.toLowerCase())
+                )
+    } else {
+      const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=food, restaurants&radius=${radius}&latitude=${userlocation.coords.latitude}&longitude=${userlocation.coords.longitude}`
+      const apiOptions = {
+        headers: {
+          Authorization: `Bearer ${YELP_API_KEY}`
+
+        }
+      }
+      return await fetch(yelpUrl, apiOptions)
+        .then((res) => res.json())
+        .then((json) =>
+          console.log(json),
+          console.log(userlocation),
+        )
     }
-    return await fetch(yelpUrl, apiOptions)
-    .then((res) => res.json())
-    .then((json) =>
-    console.log(json),
-    console.log(userAddress),
-        // setRestaurantData(
-        //   json.businesses.filter((business) =>
-        //     business.transactions.includes(activeTab.toLowerCase())
-          )
-      //   )
-      // );
+
+
+  //   if (userlocation) {
+  //     const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=food, restaurants&radius=${radius}&latitude=${userlocation.coords.latitude}&longitude=${userlocation.coords.longitude}`
+  //     const apiOptions = {
+  //       headers: {
+  //         Authorization: `Bearer ${YELP_API_KEY}`
+
+  //       }
+  //     }
+  //     return await fetch(yelpUrl, apiOptions)
+  //       .then((res) => res.json())
+  //       .then((json) =>
+  //         console.log(json),
+  //         console.log(userlocation),
+  //       )
+  //   } else {
+  //     const yelpUrl = `https://api.yelp.com/v3/businesses/search?location=${userAddress}&term=food, restaurants&radius=${radius}`
+  //     const apiOptions = {
+  //       headers: {
+  //         Authorization: `Bearer ${YELP_API_KEY}`,
+  //       },
+  //     }
+  //     //await userAddress
+  //     if (userAddress) {
+  //       console.log(userAddress)
+  //       return await fetch(yelpUrl, apiOptions)
+  //         .then((res) => res.json())
+  //         .then((json) =>
+  //           console.log(json),
+  //           console.log(userAddress),
+  //           // setRestaurantData(
+  //           //   json.businesses.filter((business) =>
+  //           //     business.transactions.includes(activeTab.toLowerCase())
+  //         )
+  //       //   )
+  //       // );
+  //     }
+  //   }
   };
 
-return (
-  <View style={styles.container}>
-    <Image style={styles.img} source={require('../assets/Feed-Your-Hangry.png')} />
-    <Text style={styles.text}>Welcome to Iffy Eats!</Text>
-    {!userlocation ? <View>
-      <Pressable
-        style={({ pressed }) => [({ backgroundColor: pressed ? 'purple' : 'hotpink' }), styles.wrapperCustom]}
-        onPress={onPressHandler}
-      >
-        <Text style={styles.btnText}>Use My Location</Text>
-      </Pressable>
-      <Text style={styles.textSpacer}>------------------- OR ------------------</Text>
-      <TextInput
-        style={styles.input}
-        keyboardType={'default'}
-        placeholder={'Enter Address'}
-      ></TextInput>
-
-      <Pressable
-        style={({ pressed }) => [({ backgroundColor: pressed ? 'purple' : 'hotpink' }), styles.wrapperCustom]}
-      >
-        <Text style={styles.btnText}>Enter Address</Text>
-      </Pressable>
-    </View> :
-      <View>
+  return (
+    <View style={styles.container}>
+      <Image style={styles.img} source={require('../assets/Feed-Your-Hangry.png')} />
+      <Text style={styles.text}>Welcome to Iffy Eats!</Text>
+      {!userlocation ? <View>
+        <Pressable
+          style={({ pressed }) => [({ backgroundColor: pressed ? 'purple' : 'hotpink' }), styles.wrapperCustom]}
+          onPress={onPressHandler}
+        >
+          <Text style={styles.btnText}>Use My Location</Text>
+        </Pressable>
+        <Text style={styles.textSpacer}>------------------- OR ------------------</Text>
         <TextInput
-          placeholder='Address'
-          onChange={e => setAddress(e.target.value)}
+          style={styles.input}
+          keyboardType={'default'}
+          placeholder={'Enter Address'}
+          value ={userAddress}
+          onChange={(e) => setUserAddress(e.target.value)}
         ></TextInput>
+
         <Pressable
           style={({ pressed }) => [({ backgroundColor: pressed ? 'purple' : 'hotpink' }), styles.wrapperCustom]}
           onPress={getYelpRestaurants}
         >
+          <Text style={styles.btnText}>Enter Address</Text>
         </Pressable>
-        <Pressable
-          style={({ pressed }) => [({ backgroundColor: pressed ? 'purple' : 'hotpink' }), styles.wrapperCustom]}
-          onPress={navToObject}
-        >
-          <Text style={styles.btnText}>Click to feed your hangry!</Text>
-        </Pressable>
-        <StatusBar style="auto" />
-      </View>
-    }
-  </View>
-)
+      </View> :
+        <View>
+          {/* <TextInput
+            placeholder='Address'
+            onChange={(e) => setUserAddress(e.target.value)}
+          ></TextInput> */}
+          {/* <Pressable
+            style={({ pressed }) => [({ backgroundColor: pressed ? 'purple' : 'hotpink' }), styles.wrapperCustom]}
+            onPress={getYelpRestaurants}
+          >
+          </Pressable> */}
+          <Pressable
+            style={({ pressed }) => [({ backgroundColor: pressed ? 'purple' : 'hotpink' }), styles.wrapperCustom]}
+            onPress={navToObject}
+          >
+            <Text style={styles.btnText}>Click to feed your hangry!</Text>
+          </Pressable>
+          <StatusBar style="auto" />
+        </View>
+      }
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
